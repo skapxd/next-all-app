c526f9dbca540ace88f550a9dc5e67b15ade1b64
manuel londoño <mLondono@q10.com>
2022-02-01 22:47:41 -0500

    feat: add BrandAppBarcomponent

---
+++ components/lv1/BrandAppBar/BrandAppBar.js
@@ -0,0 +1,6 @@
+import Style from "./BrandAppBar.module.scss";
+export function BrandAppBar(){
+    return <div
+    className={Style.BrandAppBar_Container}
+    >BrandAppBar</div>
+}
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,10 @@
+import { BrandAppBar } from "components/lv1/BrandAppBar/BrandAppBar";
 import Style from "./index.module.scss";
 export default function Home() {
-  return <div
-  className={Style.index_Scaffold}>Hola</div>;
+  return (
+    <div className={Style.index_Scaffold}>
+      <BrandAppBar />
+      Hola
+    </div>
+  );
 }
