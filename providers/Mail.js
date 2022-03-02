1cbd0b9d93b4e331c7e99d01eab4eb8780f19517
manuel londoño <mLondono@q10.com>
2022-03-02 18:53:24 -0500

    fix: fixin send email

--- providers/Mail.js
+++ providers/Mail.js
@@ -10,17 +10,29 @@ import { createTransport } from "nodemailer";
 export const sendMail = async (props) => {
   const { email, msjText, subject = "No responder a este email" } = props;
 
+  const credentials = {
+    user: process.env.USER_MAIL,
+    pass: process.env.PASS_MAIL,
+  };
+
+  console.log({ credentials });
+
   let transporter = createTransport({
     host: "smtp.gmail.com",
     port: 465,
     secure: true,
     auth: {
-      user: process.env.USER_MAIL,
-      pass: process.env.PASS_MAIL,
+      user: credentials.user,
+      pass: credentials.pass,
     },
   });
 
-  transporter.verify().then(() => {});
+  try {
+    const verify = await transporter.verify();
+    console.log({ transporterVerify: verify });
+  } catch (error) {
+    console.log({ transporterVerify: error });
+  }
 
   await transporter.sendMail({
     from: "automail.noresponder@gmail.com",
