20ca8acbf207fd2a9cdf4f88558494b3723b8581
manuel londoño <mLondono@q10.com>
2022-03-06 14:39:17 -0500

    fix: test - api/v1/auth/get-code

--- db/supabase/connection.js
+++ db/supabase/connection.js
@@ -1,7 +1,8 @@
+// @ts-check
 import { createClient } from "@supabase/supabase-js";
 
 // Create a single supabase client for interacting with your database
-const supabase_url = process.env.supabase_url;
-const supabase_key = process.env.supabase_key;
+const supabase_url = process.env.SUPABASE_URL;
+const supabase_key = process.env.SUPABASE_KEY;
 
 export const supabase = createClient(supabase_url, supabase_key);
